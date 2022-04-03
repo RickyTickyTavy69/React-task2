c9cda41967c0c79a9d1fc22512eeca913b56ef3d
Artem Bakirov <bakirovartem69@gmail.com>
2022-04-03 21:55:54 +0200

    deleted unused variable

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,28 @@
-import React, {useState} from "react";
+import React from "react";
 
 
 const Counter = (props) => {                                                                 // любой компонент в React является функцией, которая
-    const {value} = props;
+    const { value } = props;
 
-    function formatValue(){
+    function formatValue() {
         return value === 0 ? "empty" : value;
     }
 
-    function getBageClasses(){
+    function getBageClasses() {
         let classes = "badge m-2 ";
         classes += value === 0 ? 'bg-warning' : 'bg-primary';
-        return classes;  
+        return classes;
     }
 
-    return(                                                                             
+    return (
         <div>
             <span>{props.name}</span>
             <span className={getBageClasses()}>{formatValue()}</span>
-            <button className="btn btn-primary btn-sm m-2" onClick={() => {props.onIncrease(props.id)}}>+</button>
-            <button className="btn btn-primary btn-sm m-2" onClick={() => {props.onDecrease(props.id)}}>-</button>
-            <button className="btn btn-danger btn-sm m-4" onClick={() => {props.onDelete(props.id)}}>delete</button>    
+            <button className="btn btn-primary btn-sm m-2" onClick={() => { props.onIncrease(props.id) }}>+</button>
+            <button className="btn btn-primary btn-sm m-2" onClick={() => { props.onDecrease(props.id) }}>-</button>
+            <button className="btn btn-danger btn-sm m-4" onClick={() => { props.onDelete(props.id) }}>delete</button>
         </div>
-    );                                             
+    );
 }
 
 
